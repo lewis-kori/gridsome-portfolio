b4a60dee8bef7275b82d6acda1be0cd04489e0fc
Lewis <lewiskosh@gmail.com>
2020-05-14 21:17:08 +0300

    service worker v1

--- gridsome.config.js
+++ gridsome.config.js
@@ -21,8 +21,8 @@ module.exports = {
           },
           series: {
             typeName: "Series",
-            create: true
-          }
+            create: true,
+          },
         },
       },
     },
@@ -41,30 +41,41 @@ module.exports = {
       },
     },
     {
-      use: '@gridsome/plugin-google-analytics',
+      use: "@gridsome/plugin-google-analytics",
       options: {
-        id: 'UA-137370458-4'
-      }
+        id: "UA-137370458-4",
+      },
     },
     {
-      use: 'gridsome-plugin-pwa',
+      use: "gridsome-plugin-pwa",
       options: {
-          title: 'Lewis Kori',
-          startUrl: '/',
-          display: 'standalone',
-          statusBarStyle: 'default',
-          manifestPath: 'manifest.json',
-          disableServiceWorker: true,
-          serviceWorkerPath: 'service-worker.js',
-          cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg',
-          shortName: 'Gridsome',
-          themeColor: '#0a192f',
-          backgroundColor: '#172a45',
-          icon: 'src/favicon.png', // must be provided like 'src/favicon.png'
-          msTileImage: '',
-          msTileColor: '#8892b0'
-      }
-  }
+        title: "Lewis Kori",
+        startUrl: "/",
+        display: "standalone",
+        statusBarStyle: "default",
+        manifestPath: "manifest.json",
+        disableServiceWorker: true,
+        serviceWorkerPath: "service-worker.js",
+        cachedFileTypes: "js,json,css,html,png,jpg,jpeg,svg",
+        shortName: "Gridsome",
+        themeColor: "#0a192f",
+        backgroundColor: "#172a45",
+        icon: "src/favicon.png", // must be provided like 'src/favicon.png'
+        msTileImage: "",
+        msTileColor: "#8892b0",
+      },
+    },
+    {
+      use: "gridsome-plugin-service-worker",
+      options: {
+        networkFirst: {
+          routes: [
+            "/",
+            /\.(js|css|png)$/, // means "every JS, CSS, and PNG images"
+          ],
+        },
+      },
+    },
   ],
 
   transformers: {

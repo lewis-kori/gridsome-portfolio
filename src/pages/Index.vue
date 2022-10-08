91e6596e76529920eafec7d669449ed3af5287a9
Lewis Kori <39276332+lewis-kori@users.noreply.github.com>
2022-10-08 15:35:43 +0300

    update contact text

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -271,8 +271,8 @@
       <div class="columns is-centered">
         <div class="column is-half has-text-centered">
           <p>
-            My inbox is always open. Whether you have a question or just want to
-            say hi, I'll try my best to get back to you!
+           Although I’m not currently looking for any new opportunities, my inbox is always open. 
+           Whether you have a question or just want to say hi, I’ll try my best to get back to you!
           </p>
         </div>
       </div>
